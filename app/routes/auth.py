18e91f4becbec993508ae855b1829093cdb6172d
deadbrain1112 <127726225+deadbrain1112@users.noreply.github.com>
2025-12-06 18:52:42 +0700

    new

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -22,7 +22,7 @@ async def login(data: LoginRequest):
 @router.post("/register")
 async def register_account(data: RegisterRequest):
     ndt = data.ndt
-    matkhau = data.password
+    matkhau = data.matkhau
 
     # Kiểm tra email
     existed_email = await db.nha_dau_tu.find_one({"email": ndt.email})
@@ -36,7 +36,7 @@ async def register_account(data: RegisterRequest):
 
     # Chuẩn bị dữ liệu để lưu
     ndt_dict = ndt.dict(exclude_none=True)
-    ndt_dict["matkhau"] = matkhau # Nếu bạn lưu password vào DB
+    ndt_dict["matkhau"] = matkhau
 
     # Lưu vào MongoDB
     result = await db.nha_dau_tu.insert_one(ndt_dict)
